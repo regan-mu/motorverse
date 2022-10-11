0590c2e6baf82c8a380303884ba2376e81b27eb4
Regan254 <40056428+regan254@users.noreply.github.com>
2022-10-11 16:30:18 +0300

    Delete Vehicle & Update vehicle

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -278,20 +278,191 @@ exports.createPost = [
 ]
 
 // Delete
-exports.deleteGet = (req, res) => {
-    res.send("Delete Vehicle Get");
+exports.deleteGet = (req, res, next) => {
+    Vehicle.findById(req.params.id).exec((err, foundVehicle) => {
+        if (err) {
+            return next(err);
+        }
+        if (foundVehicle) {
+            res.render("deleteVehicle", {title: "Delete Vehicle", vehicle: foundVehicle});
+        } else {
+            const vehicleErr = new Error()
+            vehicleErr.message = "Vehicle not found"
+            vehicleErr.status = 404
+            vehicleErr.name = "Not Found"
+            next(vehicleErr);
+        }
+    });
 }
-exports.deletePost = (req, res) => {
-    res.send("Delete Vehicle post");
+exports.deletePost = (req, res, next) => {
+    if (req.body.password === "Regan123App") {
+        Vehicle.findByIdAndRemove(req.body.id).exec((err, removed) => {
+            if (err) {
+                return next(err);
+            }
+            if (removed) {
+                res.redirect("/");
+            } else {
+                res.redirect(`/motorverse/vehicle${res.body.id}/delete`);
+            }
+
+        })
+    } else {
+        const passwordErr = new Error();
+        passwordErr.message = "Enter correct password";
+        passwordErr.status = 401;
+        passwordErr.name = "Unauthorized";
+        next(passwordErr);
+    }
 }
 
 // update
-exports.updateGet = (req, res) => {
-    res.send("Update Vehicle Get");
+exports.updateGet = (req, res, next) => {
+    Vehicle.findById(req.params.id).exec((err, foundVehicle) => {
+        if (err) {
+            next(err);
+        }
+        if (foundVehicle) {
+            async.parallel(
+                {
+                    makes(callback) {
+                        Manufacturer.find({}, "name").sort({name: 1}).exec(callback)
+                    },
+                    categories(callback) {
+                        Category.find({}, "name").sort({name: 1}).exec(callback)
+                    }
+                },
+                (error, results) => {
+                    if (error) {
+                        return next(error);
+                    }
+                    res.render(
+                        "vehicleCreate", 
+                        {
+                            title: "Update Vehicle",
+                            category: results.categories,
+                            make: results.makes,
+                            query: foundVehicle
+                        }
+                    );
+                }
+            );
+        } else {
+            const vehicleErr = new Error()
+            vehicleErr.message = "Vehicle not found"
+            vehicleErr.status = 404
+            vehicleErr.name = "Not Found"
+            next(vehicleErr);
+        }
+    });
 }
-exports.updatePost = (req, res) => {
-    res.send("Update Vehicle post");
-}
+exports.updatePost = [
+        body("model", "Vehicle model required")
+            .trim()
+            .isLength({min: 2})
+            .escape(),
+        body("yom", "Year of manufacture required")
+            .trim()
+            .isLength({min: 1})
+            .escape(),
+        body("location", "Location Required")
+            .trim()
+            .isLength({min: 2})
+            .escape(),
+        body("mileage", "Mileage required")
+            .trim()
+            .isLength({min:1})
+            .escape(),
+        body("category", "Must select category")
+            .trim()
+            .isLength({min: 2})
+            .escape(),
+        body("doors", "Number of doors required")
+            .trim()
+            .isLength({min: 1})
+            .escape(),
+        body("engine", "Engine Capacity required")
+            .trim()
+            .isLength({min: 1})
+            .escape(),
+        body("make", "Select manufacturer")
+            .trim()
+            .isLength({min: 1})
+            .escape(),
+        body("price", "Vehicle price required")
+            .trim()
+            .isLength({min: 4})
+            .escape(),
+        body("imageurl", "Image url required")
+            .trim()
+            .isLength({min: 1}),
+        body("description", "Vehicle Description required")
+            .trim()
+            .isLength({min: 1})
+            .escape()
+        ,
+        (req, res, next) => {
+            const errors = validationResult(req)
+            query = {
+                model: req.body.model,
+                yom: req.body.yom,
+                color: req.body.color,
+                location: req.body.location,
+                mileage: req.body.mileage,
+                category: req.body.category,
+                doors: req.body.doors,
+                engine_capacity: req.body.engine,
+                manufacturer: req.body.make,
+                price: req.body.price,
+                imageUrl: req.body.imageurl,
+                description: req.body.description,
+                _id: req.params.id
+            }
+            const vehicle = new Vehicle(query);
+    
+            if (!errors.isEmpty()) {
+                async.parallel(
+                    {
+                        makes(callback) {
+                            Manufacturer.find({}, "name").exec(callback)
+                        },
+                        categories(callback) {
+                            Category.find({}, "name").exec(callback)
+                        }
+                    },
+                    (error, results) => {
+                        if (error) {
+                            return next(error);
+                        }
+                        res.render(
+                            "vehicleCreate", 
+                            {
+                                title: "Add Vehicle",
+                                category: results.categories,
+                                make: results.makes,
+                                query: query
+                            }
+                        );
+                    }
+                );
+            } else {
+                if (req.body.password.trim() === "Regan123App") {
+                    Vehicle.findByIdAndUpdate(req.params.id, vehicle).exec(err => {
+                        if(err) {
+                            return next(err);
+                        }
+                        res.redirect(vehicle.url);
+                    })
+                } else {
+                    const passwordErr = new Error();
+                    passwordErr.message = "Enter correct password";
+                    passwordErr.status = 401;
+                    passwordErr.name = "Unauthorized";
+                    next(passwordErr)
+                }
+            }
+        }
+]
 
 
 
