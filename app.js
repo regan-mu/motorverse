ebaf86095d9415091098822040a561b5f061d690
Regan254 <40056428+regan254@users.noreply.github.com>
2022-10-13 00:19:49 +0300

    Change .gitignore file

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const compression = require("compression");
-const helmet = require("helmet")
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const motorverse = require("./routes/motorverse");
