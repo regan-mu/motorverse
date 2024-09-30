9be732ec85adcbb0fd4b6c3800cc27b11525436b
regan-mu <regansomi@gmail.com>
2024-09-30 08:41:06 +0300

    Add manufacturer button

--- createDB.js
+++ createDB.js
@@ -1,24 +1,14 @@
-#! /usr/bin/env node
+console.log('This script populates some vehicles');
 
-console.log('This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true');
-
-// Get arguments passed on command line
-var userArgs = process.argv.slice(2);
-/*
-if (!userArgs[0].startsWith('mongodb')) {
-    console.log('ERROR: You need to specify a valid mongodb URL as the first argument');
-    return
-}
-*/
 const async = require('async')
 const Vehicle = require('./models/Vehicle')
 const Category = require('./models/Category')
 const Manufacturer = require('./models/Manufacturer')
+require("dotenv").config();
 
 
 const mongoose = require('mongoose');
-const mongoDB = userArgs[0];
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -50,7 +40,7 @@ function createVehicle(model, yom, color, location,  mileage, category, doors, d
         }
         console.log("New Vehicle: " + vehicle);
         vehicles.push(vehicle)
-        cb(null, book)
+        cb(null, vehicle)
     });
 }
 
@@ -101,7 +91,7 @@ function vehiclesAdd(cb) {
                     2016, 
                     "Orange",
                     "Mombasa",
-                    0000, 
+                    0, 
                     categories[1], 
                     4, 
                     "Audi Q5 is a dream car and a must own for me", 
